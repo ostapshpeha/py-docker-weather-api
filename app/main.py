c7d9cca78b90dc6990146a6a6c20f21969521f24
Ostap Shp <stark171705@gmail.com>
2026-01-07 18:01:39 +0200

    completed, fixed flake

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ import requests
 def get_weather() -> None:
     secret_key = os.getenv("API_KEY")
     if not secret_key:
-        print("CRITICAL ERROR: API_KEY is missing in environment variables!", file=sys.stderr)
+        print(
+            "CRITICAL ERROR: API_KEY is missing in environment variables!",
+            file=sys.stderr
+        )
         sys.exit(1)
 
     location = "Paris"
@@ -28,14 +31,17 @@ def get_weather() -> None:
 
         data = response.json()
 
-
-        current_temp = data['current']['temp_c']
-        condition = data['current']['condition']['text']
-        print(f"SUCCESS: Weather in {location}: {current_temp}°C, {condition}", flush=True)
+        current_temp = data["current"]["temp_c"]
+        condition = data["current"]["condition"]["text"]
+        print(
+            f"SUCCESS: Weather in {location}: {current_temp}°C, {condition}",
+            flush=True
+        )
 
     except requests.exceptions.RequestException as e:
         print(f"REQUEST FAILED: {e}", file=sys.stderr)
         sys.exit(1)
+
 
 if __name__ == "__main__":
     get_weather()
